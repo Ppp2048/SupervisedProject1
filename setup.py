b5f2ab8e1ccf9c34a29961cfa6c39b32fee44226
Ppp2048 <pabakpany@gmail.com>
2026-03-14 01:14:37 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="SupervisedProject1",
+    version="0.0.1",
+    author="Pabak",
+    author_email="pabakpany@gmail.com",
+    packages=find_packages(),
+)
